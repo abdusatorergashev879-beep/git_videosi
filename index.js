e86f6abb94413e5a353c724d340b6917f92338e9
Muhammadjon <abdusatorergashev879@gmail.com>
2026-05-16 18:22:20 +0500

    i am create index .html file

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birinchi loyiham</title>
+</head>
+<body>
+   <h1>salom</h1>
+  <p>bu sayt juda yahshi</p>
+  <hr>
+  <br>
+  <h1>Ronaldo</h1>
+</body>
+</html>
